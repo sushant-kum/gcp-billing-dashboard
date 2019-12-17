20254456ee33581ba1af84d1195bbee702f4c5c3
Sushant Kumar <sushant.kum96@gmail.com>
2019-12-17 18:38:37 +0530

    ...

--- src/app/pages/server-costs/server-costs-reports/server-costs-reports.component.ts
+++ src/app/pages/server-costs/server-costs-reports/server-costs-reports.component.ts
@@ -1,26 +1,70 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MAT_MOMENT_DATE_ADAPTER_OPTIONS, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+
+import * as _moment from 'moment';
+// tslint:disable-next-line:no-duplicate-imports
+import { default as _rollupMoment } from 'moment';
 
 /* Component Imports */
 import { SidebarComponent } from 'src/app/components/sidebar/sidebar.component';
 
 /* Services Imports */
 import { HeaderService } from 'src/app/services/header/header.service';
+import { RegexService } from 'src/app/services/regex/regex.service';
 
 /* Config Imports */
 import { Config } from 'src/app/configs/config';
 
+const moment = _rollupMoment || _moment;
+
+export const DATE_PICKER_FORMATS = {
+  parse: {
+    dateInput: 'DD MMM, YYYY'
+  },
+  display: {
+    dateInput: 'DD MMM, YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'DD MMM, YYYY',
+    monthYearA11yLabel: 'MMMM YYYY'
+  }
+};
+
 const PAGE_ID = 'server-costs-reports';
+
 @Component({
   selector: 'app-server-costs-reports',
   templateUrl: './server-costs-reports.component.html',
-  styleUrls: ['./server-costs-reports.component.scss']
+  styleUrls: ['./server-costs-reports.component.scss'],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+    },
+
+    { provide: MAT_DATE_FORMATS, useValue: DATE_PICKER_FORMATS }
+  ]
 })
 export class ServerCostsReportsComponent implements OnInit {
   private _page_id = PAGE_ID;
   config: Config = new Config();
 
-  constructor(private _title: Title, private _header_service: HeaderService, public sidebar: SidebarComponent) {}
+  form_filters: FormGroup = new FormGroup({
+    duration: new FormControl('current_month', [Validators.required]),
+    duration_start: new FormControl(moment(), [Validators.pattern(this._regex_service.date_dd_mmm_yyyy)]),
+    duration_end: new FormControl(moment(), [Validators.pattern(this._regex_service.date_dd_mmm_yyyy)]),
+    group_by: new FormControl()
+  });
+
+  constructor(
+    private _title: Title,
+    private _header_service: HeaderService,
+    private _regex_service: RegexService,
+    public sidebar: SidebarComponent
+  ) {}
 
   ngOnInit() {
     this._title.setTitle(this.config.page_map[this._page_id].name + ' - ' + this.config.app_title);
--- src/app/pages/server-costs/server-costs-reports/server-costs-reports.module.ts
+++ src/app/pages/server-costs/server-costs-reports/server-costs-reports.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 /* Angular Mateerial Imports */
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
 
 /* Fontawesome Imports */
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 /* Solid Icons */
-import {} from '@fortawesome/free-solid-svg-icons';
+import { faArrowsAltH as fasArrowsAltH, faCalendarDay as fasCalendarDay } from '@fortawesome/free-solid-svg-icons';
 /* Regular Icons */
 import {} from '@fortawesome/free-regular-svg-icons';
 /* Brand Icons */
@@ -19,8 +25,16 @@ import { ServerCostsReportsComponent } from './server-costs-reports.component';
 @NgModule({
   declarations: [ServerCostsReportsComponent],
   imports: [
+    /* Angular Imports */
+    ReactiveFormsModule,
+
     /* Angular Material Imports */
     MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatDividerModule,
+    MatDatepickerModule,
+    MatInputModule,
 
     /* Font-awesome Impors */
     FontAwesomeModule,
@@ -33,7 +47,7 @@ import { ServerCostsReportsComponent } from './server-costs-reports.component';
 export class ServerCostsReportsModule {
   constructor(fa_icon_library: FaIconLibrary) {
     // Include solid fa icons
-    // fa_icon_library.addIcons(fasHeart);
+    fa_icon_library.addIcons(fasArrowsAltH, fasCalendarDay);
     // Include regular fa icons
     // Include brand fa icons
   }
